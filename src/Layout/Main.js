806986717aec2e3d8c043efb0a6917c87393e133
Saidur Rahman <saidurrahman1473@gmail.com>
2022-11-08 00:59:51 +0600

    add header

--- src/Layout/Main.js
+++ src/Layout/Main.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Footer from '../Pages/Shered/Footer/Footer';
+import Header from '../Pages/Shered/Header/Header';
 
 const Main = () => {
     return (
         <div>
+            <Header></Header>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
